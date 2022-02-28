3f21b64e4a50b6b785af25a3d53c02c5f11c6592
Tu Nguyen ECOE <tunta@pitek.one>
2022-02-28 17:14:53 +0700

    add nav-bar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -27,16 +27,72 @@ const Navbar = ({ navWhite }) => {
           <Burger fill={navWhite ? "#fff" : "#002266"} />
         </div>
       </div>
-      {navShow && (
-        <div className={`${styles.navMenu} ${styles.navShow}`}>
-          <div className={styles.menuRed}></div>
-          <div className={styles.menuBlue}>
-            <div className={styles.close} onClick={() => setNavShow(false)}>
-              <Image src={"/icons/close.svg"} width={40} height={40} alt="" />
+      <div className={`${styles.navMenu} ${navShow && styles.navShow} `}>
+        <div className={`${styles.menuRed} ${navShow && styles.navShow}`}>
+          <div className={styles.arrowPatterns}>
+            <Image
+              src={"/icons/arrow-pattern-white-160x160.svg"}
+              width={160}
+              height={160}
+              alt=""
+            />
+          </div>
+          <div className={styles.logoMenu}>
+            <Image
+              src={"/icons/pitek_logo_white.svg"}
+              width={240}
+              height={112}
+              alt=""
+            />
+          </div>
+
+          <p className={styles.desc}>
+            Starting operations in early 2021, Pitek brings products and
+            services of modern technology, helping customers who’re approaching
+            new technology trends have effective experiences, tailored to their
+            needs.
+          </p>
+
+          <div className={styles.sendMessage}>
+            <p>SAY HELLO</p>
+            <a className="link-btn-white" href="#">
+              SEND A MESSAGE
+            </a>
+          </div>
+
+          <div className={styles.navFooter}>
+            <div className={styles.address}>
+              <p>PITEK JSC ©2022</p>
+              <p>
+                633 - 635 Dien Bien Phu, Ward 25, <br /> Binh Thanh District, Ho
+                Chi Minh.
+              </p>
+            </div>
+            <div className={styles.contact}>
+              <p>GET IN TOUCH</p>
+              <p>+84 86 900 22 44</p>
+              <p> info@pitek.one</p>
             </div>
           </div>
         </div>
-      )}
+        <div className={`${styles.menuBlue} ${navShow && styles.navShow}`}>
+          <div className={styles.circlePatterns}>
+            <Image
+              src={"/home-page/circle-pattern-320x320.svg"}
+              width={320}
+              height={320}
+              alt=""
+            />
+          </div>
+          <div className={styles.close} onClick={() => setNavShow(false)}>
+            <Image src={"/icons/close.svg"} width={40} height={40} alt="" />
+          </div>
+          <h2>ABOUT</h2>
+          <h2>SERVICE</h2>
+          <h2>PROJECTS</h2>
+          <h2>CONTACT</h2>
+        </div>
+      </div>
     </>
   );
 };
